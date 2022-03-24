423aca1a9f8083045abe7dca0e2a7efaf203eef4
asmyasnikov <79263256394@ya.ru>
2022-03-25 00:09:18 +0300

    latest ydb-go-sdk + retry traces

--- retry.go
+++ retry.go
@@ -1,24 +1,26 @@
 package tracing
 
 import (
+	otlog "github.com/opentracing/opentracing-go/log"
+
 	"github.com/ydb-platform/ydb-go-sdk/v3/trace"
 )
 
 func Retry(details trace.Details) (t trace.Retry) {
 	if details&trace.RetryEvents != 0 {
 		t.OnRetry = func(info trace.RetryLoopStartInfo) func(trace.RetryLoopIntermediateInfo) func(trace.RetryLoopDoneInfo) {
-			//start := startSpan(
-			//	info.Context,
-			//	"ydb_retry",
-			//)
-			//start.SetTag("idempotent", info.Idempotent)
+			start := startSpan(
+				info.Context,
+				"ydb_retry",
+			)
+			start.SetTag("idempotent", info.Idempotent)
 			return func(info trace.RetryLoopIntermediateInfo) func(trace.RetryLoopDoneInfo) {
-				//intermediate(start, info.Error)
+				intermediate(start, info.Error)
 				return func(info trace.RetryLoopDoneInfo) {
-					//finish(start,
-					//	info.Error,
-					//	otlog.Int("attempts", info.Attempts),
-					//)
+					finish(start,
+						info.Error,
+						otlog.Int("attempts", info.Attempts),
+					)
 				}
 			}
 		}
